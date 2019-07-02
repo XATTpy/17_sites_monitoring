ceeb307480d4f3990b481abd475b1ab5fa723bae
XATTpy <zoomerrr@mail.ru>
2019-07-02 15:51:42 +0300

    add new variable

--- check_sites_health.py
+++ check_sites_health.py
@@ -11,6 +11,13 @@ def get_args():
         type=str,
         help='Input path to textfile with URLs.'
     )
+    parser.add_argument(
+        '-d',
+        '--days',
+        default=30,
+        type=int,
+        help='Input number of days to check payment.'
+    )
     args = parser.parse_args()
     return args, parser
 
@@ -21,7 +28,7 @@ def load_urls4check(path, parser):
 
 
 
-def is_server_respond_with_200(url):
+def is_server_respond_with_2xx(url):
     try:
         response = requests.get(url)
         return response.ok
@@ -31,14 +38,16 @@ def is_server_respond_with_200(url):
 
 def get_domain_expiration_date(url):
     response = whois(url)
-    expiration_date = response.expiration_date[1]
+    expiration_date = response.expiration_date
+    if type(expiration_date) == list:
+        expiration_date = expiration_date[1]
     return expiration_date
 
 
-def is_domain_paid(expiration_date):
+def is_domain_paid(expiration_date, days):
     now = datetime.now()
     difference = expiration_date - now
-    delta = timedelta(days=30)
+    delta = timedelta(days)
     if difference >= delta:
         return 'Paid'
     else:
@@ -48,16 +57,17 @@ def is_domain_paid(expiration_date):
 if __name__ == '__main__':
     args, parser = get_args()
     path = args.input
+    days = args.days
     try:
         urls = load_urls4check(path, parser)
     except FileNotFoundError:
         parser.error('File not found.')
 
     for url in urls:
-        response = is_server_respond_with_200(url)
+        response = is_server_respond_with_2xx(url)
         if response:
             expiration_date = get_domain_expiration_date(url)
-            payment = is_domain_paid(expiration_date)
+            payment = is_domain_paid(expiration_date, days)
             status = 'URL: {} , status: OK, {}'.format(url, payment)
         else:
             status = 'URL: {} , status: No connection'.format(url)
